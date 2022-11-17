da8e6765f1d1befee65874295aa43e9485c806ac
ali-narimanov <ali.narimanli@mail.ru>
2022-11-17 15:46:14 +0400

    #Coommit

--- asssets/js/main.js
+++ asssets/js/main.js
@@ -1,143 +1,62 @@
-let li1 = document.querySelector('.li-1');
-let li2 = document.querySelector('.li-2');
-let li3 = document.querySelector('.li-3');
-let li4 = document.querySelector('.li-4');
-let li5 = document.querySelector('.li-5');
-let li6 = document.querySelector('.li-6');
-let li7 = document.querySelector('.li-7');
-let li8 = document.querySelector('.li-8');
-//-----------
+const rubl=document.getElementById("rubl");
+const dollar=document.getElementById("dollar");
+const euro=document.getElementById("euro");
+const funts=document.getElementById("funts");
+const rubl1= document.getElementById('rubl1');
+const dollar1= document.getElementById('dollar1');
+const euro1 = document.getElementById('euro1');
+const funst1 = document.getElementById('funts1')
+
 let enterBtn = document.querySelector('.btn1');
-//-----------
-let input1 = document.querySelector('.input1');
-let input2 = document.querySelector('.input2');
-//-----------
-
-//--lis' style changes
-li1.addEventListener('click', ()=>{
-    li1.style.backgroundColor="#833AE0"
-    li1.style.color="white"
-    li2.style.color="black";
-    li2.style.backgroundColor="white"
-    li3.style.backgroundColor= "white";
-    li3.style.color="black"
-    li4.style.backgroundColor="white"
-    li4.style.color="black"
-})
-
-li2.addEventListener('click',()=>{
-    li2.style.backgroundColor="#833AE0"
-    li2.style.color="white"
-    li1.style.color="black";
-    li1.style.backgroundColor="white"
-    li3.style.backgroundColor="white"
-    li3.style.color="black";
-    li4.style.backgroundColor="white"
-    li4.style.color="black"
-    
-})
-li3.addEventListener('click',()=>{
-    li3.style.backgroundColor="#833AE0"
-    li3.style.color="white"
-    li1.style.color="black";
-    li1.style.backgroundColor="white"
-    li2.style.color="black";
-    li2.style.backgroundColor="white"
-    li4.style.backgroundColor="white"
-    li4.style.color="black"
-})
-li4.addEventListener('click',()=>{
-    li4.style.backgroundColor="#833AE0"
-    li4.style.color="white"
-    li1.style.color="black";
-    li1.style.backgroundColor="white"
-    li2.style.color="black";
-    li2.style.backgroundColor="white"
-    li3.style.backgroundColor="white"
-    li3.style.color="black";
-})
-    
-
-li5.addEventListener('click', ()=>{
-    li5.style.backgroundColor="#833AE0"
-    li5.style.color="white"
-    li6.style.color="black";
-    li6.style.backgroundColor="white"
-    li7.style.backgroundColor= "white";
-    li7.style.color="black"
-    li8.style.backgroundColor="white"
-    li8.style.color="black"
-})
-
-li6.addEventListener('click', ()=>{
-    li6.style.backgroundColor="#833AE0"
-    li6.style.color="white"
-    li5.style.color="black";
-    li5.style.backgroundColor="white"
-    li7.style.backgroundColor= "white";
-    li7.style.color="black"
-    li8.style.backgroundColor="white"
-    li8.style.color="black"
-});
-
-
-li7.addEventListener('click', ()=>{
-    li7.style.backgroundColor="#833AE0"
-    li7.style.color="white"
-    li5.style.color="black";
-    li5.style.backgroundColor="white"
-    li6.style.backgroundColor= "white";
-    li6.style.color="black"
-    li8.style.backgroundColor="white"
-    li8.style.color="black"
-});
-li8.addEventListener('click', ()=>{
-    li8.style.backgroundColor="#833AE0"
-    li8.style.color="white"
-    li5.style.color="black";
-    li5.style.backgroundColor="white"
-    li7.style.backgroundColor= "white";
-    li7.style.color="black"
-    li6.style.backgroundColor="white"
-    li6.style.color="black"
-});
-
-
-//--lis' style changes
-
-
-
-
-
-// начальное значение рубля в доллары
+const input1 = document.getElementsByName('first-input');
+const input2 = document.getElementsByName('second-input');
+let ul1 = document.querySelector('.ul1');
+let ul2 = document.querySelector('.ul2')
 let h2= document.querySelector('.text2')
 let h1= document.querySelector('.text1')
-  fetch("https://api.exchangerate.host/latest?base=RUB&symbols=USD")
-  .then(resp => resp.json())
-  .then(data => {
-   
-   let a = data.rates.USD
-   console.log(a)
+const one = document.getElementsByName('one');
+const two = document.getElementsByName('two');
+function currencyChange(e){
+    if(e.target.className == "currency" || e.target.className=='currency1'){
+        const changes=[...e.target.parentElement.children];
+        changes.forEach(item=>{
+            if(item.classList.contains('change')){
+                item.classList.remove('change');
+            }
+        })
+        e.target.classList.add('change')
+    };
+}
+rubl.addEventListener("click",currencyChange);
+dollar.addEventListener("click",currencyChange);
+euro.addEventListener('click',currencyChange);
+funts.addEventListener('click',currencyChange);
+rubl1.addEventListener("click",currencyChange);
+dollar1.addEventListener("click",currencyChange);
+euro1.addEventListener('click',currencyChange);
+funst1.addEventListener('click',currencyChange);
 
-h1.innerHTML= "1 RUB = " + a + " USD";
-    })  
-    fetch("https://api.exchangerate.host/latest?base=USD&symbols=RUB")
-    .then(resp1 => resp1.json())
-    .then(data1 => {
-     
-     let b = data1.rates.RUB
-     console.log(b)
-  
-  h2.innerHTML= "1 USD = " + b + " RUB";
-  
-      })  
-   // начальное значение рубля в доллары
 
- function changCurr(){
-    if(
-        li2.style.color = 'white' ){
-        h1.innerHTML='123';
-    }
- }
 
-   
+const p1=document.querySelector('.text1');
+const p2 = document.querySelector('.text2');
+    
+// input1.addEventListener('keypress',()=>{
+//     one.forEach(item.classList.contains('change')){
+//         first = item.innerText;
+
+//     };
+//     two.forEach(item=>{
+//         if(item.classList.contains('change')){
+//             second = item.innerText
+//         }
+//     })
+//     fetch(`https://api.exchangerate.host/latest?base=${first}&symbols=${second}`)
+//     .then(r=>r.json())
+//     .then((data)=>{
+//         input2.value=Number(input1.value)*data.rates[second];
+//         p1.innerText=`1 ${first} = ${data.rates[second]} ${second}`;
+//         p2.innerText=`1 ${second} = ${1/data.rates[second]} ${first}`
+        
+//     });
+// });
