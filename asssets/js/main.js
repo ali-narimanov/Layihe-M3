e123a40d7a9156e07d114c64e70ad910730ef1a8
ali-narimanov <ali.narimanli@mail.ru>
2022-11-17 19:31:39 +0400

    #8commit

--- asssets/js/main.js
+++ asssets/js/main.js
@@ -8,8 +8,8 @@ const euro1 = document.getElementById('euro1');
 const funst1 = document.getElementById('funts1')
 
 let enterBtn = document.querySelector('.btn1');
-const input1 = document.getElementsByName('first-input');
-const input2 = document.getElementsByName('second-input');
+const input1 = document.getElementsByName('first-input')[0];
+const input2 = document.getElementsByName('second-input')[0];
 let ul1 = document.querySelector('.ul1');
 let ul2 = document.querySelector('.ul2')
 let h2= document.querySelector('.text2')
@@ -41,22 +41,63 @@ funst1.addEventListener('click',currencyChange);
 const p1=document.querySelector('.text1');
 const p2 = document.querySelector('.text2');
     
-// input1.addEventListener('keypress',()=>{
-//     one.forEach(item.classList.contains('change')){
-//         first = item.innerText;
 
-//     };
-//     two.forEach(item=>{
-//         if(item.classList.contains('change')){
-//             second = item.innerText
-//         }
-//     })
-//     fetch(`https://api.exchangerate.host/latest?base=${first}&symbols=${second}`)
-//     .then(r=>r.json())
-//     .then((data)=>{
-//         input2.value=Number(input1.value)*data.rates[second];
-//         p1.innerText=`1 ${first} = ${data.rates[second]} ${second}`;
-//         p2.innerText=`1 ${second} = ${1/data.rates[second]} ${first}`
+
+let first;
+let second;
+input1.addEventListener('keypress',()=>{
+   one.forEach(item=>{
+    if(item.classList.contains('change')){
         
-//     });
-// });
+        first = item.innerText
+    }
+   })
+
+    two.forEach(item=>{
+        if(item.classList.contains('change')){
+            
+            second=item.innerText
+        }
+    });
+    
+    
+    fetch(`https://api.exchangerate.host/latest?base=${first}&symbols=${second}`)
+    .then(r=>r.json())
+    .then((data)=>{
+        
+        input2.value=Number(input1.value)*data.rates[second];
+        p1.innerText=`1 ${first} = ${data.rates[second]} ${second}`;
+        p2.innerText=`1 ${second} = ${1/data.rates[second]} ${first}`;
+        
+    })
+})
+
+
+
+
+input2.addEventListener('keypress',()=>{
+    one.forEach(item=>{
+     if(item.classList.contains('change')){
+         
+         first = item.innerText
+     }
+    })
+ 
+     two.forEach(item=>{
+         if(item.classList.contains('change')){
+             
+             second=item.innerText
+         }
+     });
+     
+     
+     fetch(`https://api.exchangerate.host/latest?base=${second}&symbols=${first}`)
+     .then(r=>r.json())
+     .then((data)=>{
+         
+         input1.value=Number(input2.value)*data.rates[first];
+         p1.innerText=`1 ${first} = ${1/data.rates[first]} ${second}`;
+         p2.innerText=`1 ${second} = ${data.rates[first]} ${first}`;
+         
+     })
+ })
